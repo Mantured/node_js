ab7ff03252f02fab3b4dfc44c4ff781bbcfa6b68
Manuel Gerolin <manuel31gerolin@gmail.com>
2023-02-03 17:50:34 +0100

    new models

--- the_basic/app.js
+++ the_basic/app.js
@@ -1,35 +1,28 @@
 const http = require('http');
 const path = require('path');
 
-const bodyParser = require('body-parser');
-
 const express = require('express');
-//const expressHbs = require('express-handlebars')
+const bodyParser = require('body-parser');
 
 const app = express();
 
-
-/* app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts', 
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-})); */
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const errorController = require('./controllers/error')
+
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/admin',adminData.routes);
-app.use(shopRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use((req, res, next) =>{
-    res.status(404).render('404', {pageTitle: 'Page not Found'});
-});
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+
+app.use(errorController.get404);
 
 
 app.listen(3000);
