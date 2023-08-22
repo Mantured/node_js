dd637f1a6d1e066c05c167ac18a9a9318cde55f3
Manuel Gerolin <manuel31gerolin@gmail.com>
2023-08-22 14:57:37 +0200

    WIP

--- the_basic/app.js
+++ the_basic/app.js
@@ -4,15 +4,18 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const errorController = require('./controllers/error')
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -24,5 +27,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+
+sequelize.sync().then(result =>{
+    console.log(result)
+}).catch(err => {
+    console.log(err);
+});
 
 app.listen(3000);
